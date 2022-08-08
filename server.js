a243fef9c75d511533f9b3ccd7315f14e23e5c18
Patrick McPartlan <60454641+patrickmcpartlan@users.noreply.github.com>
2022-08-08 08:40:21 -0500

    Update2

--- server.js
+++ server.js
@@ -1,37 +1,55 @@
-
 const express = require('express');
-const request = requrie('requests');
-
-
 const app = express();
-
-
-
-app.get('/api', (req, res, body) => {
-	let name = req.query.name;
-	
-	let feature = req.query.feature;
-	
-
-	var obj = {
-		"keyOne": name,
-		"keyTwo": feature
-	}
-
-	let something = "asdfasdfasdfa sdfas dfasdfjjj"
-	request.post('your geoserver location', something)
-
-
-
-
-	
-	res.send(feature);
-
-}) 
-
-
-
-
-app.listen('3000', () => {
-	console.log('application running on port 3000');
+ 
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .send('Hello server is running')
+    .end();
 });
+ 
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+
+
+
+// const express = require('express');
+// const request = requrie('requests');
+
+
+// const app = express();
+
+
+
+// app.get('/api', (req, res, body) => {
+// 	let name = req.query.name;
+	
+// 	let feature = req.query.feature;
+	
+
+// 	var obj = {
+// 		"keyOne": name,
+// 		"keyTwo": feature
+// 	}
+
+// 	let something = "asdfasdfasdfa sdfas dfasdfjjj"
+// 	request.post('your geoserver location', something)
+
+
+
+
+	
+// 	res.send(feature);
+
+// }) 
+
+
+
+
+// app.listen('3000', () => {
+// 	console.log('application running on port 3000');
+// });

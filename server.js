4705237b7be33efd092601967ff0133719e52413
Patrick McPartlan <60454641+patrickmcpartlan@users.noreply.github.com>
2022-08-14 00:19:34 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const app = express();
+const port = 3000;
  
 app.get('/', (req, res) => {
   res
-    .status(200)
     .send('Hello pp server is running')
-    .end();
 });
  
 // Start the server
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`App listening on port ${PORT}`);
   console.log('Press Ctrl+C to quit.');
 });
